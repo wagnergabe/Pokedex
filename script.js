42ca4f1f61b1564bec99e3e20577088cfa50a970
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-07-04 18:37:21 -0500

    changed colors array around for more accurate type depictions

--- script.js
+++ script.js
@@ -1,67 +1,64 @@
-const poke_container = document.getElementById('poke-container');
+const poke_container = document.getElementById("poke-container");
 const pokemon_count = 150;
 const colors = {
-      normal: '#A8A77A',
-        fire: '#EE8130',
-        water: '#6390F0',
-        electric: '#F7D02C',
-        grass: '#7AC74C',
-        ice: '#96D9D6',
-        fighting: '#C22E28',
-        poison: '#A33EA1',
-        ground: '#E2BF65',
-        flying: '#A98FF3',
-        psychic: '#F95587',
-        bug: '#A6B91A',
-        rock: '#B6A136',
-        ghost: '#735797',
-        dragon: '#6F35FC',
-        dark: '#705746',
-        steel: '#B7B7CE',
-        fairy: '#D685AD',
-        //https://stackoverflow.com/questions/70786719/cycling-through-an-array-and-finding-the-key-that-a-pokemon-type
-    };
+    dragon: "#6F35FC",
+  bug: "#A6B91A",
+  ghost: "#735797",
+  fire: "#EE8130",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  rock: "#B6A136",
+  water: "#6390F0",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
+  normal: "#A8A77A",
+  //https://stackoverflow.com/questions/70786719/cycling-through-an-array-and-finding-the-key-that-a-pokemon-type
+};
 
-    const main_types = Object.keys(colors)
-  
+const main_types = Object.keys(colors);
 
-    const fetchPokemons = async() => {
-        for(let i = 1; i <= pokemon_count; i++) {
-            await getPokemon(i);
-        }
-    }
+const fetchPokemons = async () => {
+  for (let i = 1; i <= pokemon_count; i++) {
+    await getPokemon(i);
+  }
+};
 
-    const getPokemon = async(id) => {
-        const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-        const res = await fetch(url);
-        const data = await res.json();
-        createPokemonCard(data)
-    }   
-        createPokemonCard = (pokemon) => {
-            const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  createPokemonCard(data);
+};
+createPokemonCard = (pokemon) => {
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
 
-            id = ""
+  id = "";
 
-            if(pokemon.id < 10) {
-                id = "00" + pokemon.id
-            } else if(pokemon.id < 100) {
-                id = "0" + pokemon.id
-            } else {
-                id = pokemon.id
-            };
+  if (pokemon.id < 10) {
+    id = "00" + pokemon.id;
+  } else if (pokemon.id < 100) {
+    id = "0" + pokemon.id;
+  } else {
+    id = pokemon.id;
+  }
 
-            const poke_types = pokemon.types.map(type => type.type.name)
-            const type = main_types.find(type => poke_types.indexOf(type) > -1)
-       
+  const poke_types = pokemon.types.map((type) => type.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
 
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+  const color = colors[type];
 
-            const pokemonEl = document.createElement('div');
-            pokemonEl.classList.add('pokemon');
-            const color = colors[type];
+  pokemonEl.style.backgroundColor = color;
 
-            pokemonEl.style.backgroundColor = color
-           
-            const pokemonInnerHTML = `    
+  const pokemonInnerHTML = `    
         
             <div class="img-container">
               <img src="${pokemon.sprites.front_default}" />
@@ -72,12 +69,11 @@ const colors = {
               <small class="type">Type: <span>${type}</span></small>
             </div>
           </div>
-        `
+        `;
 
-        pokemonEl.innerHTML = pokemonInnerHTML;
+  pokemonEl.innerHTML = pokemonInnerHTML;
 
-        poke_container.appendChild(pokemonEl)
-        }
-    
+  poke_container.appendChild(pokemonEl);
+};
 
-    fetchPokemons();
+fetchPokemons();
