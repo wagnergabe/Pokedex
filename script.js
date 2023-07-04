6a1692c1d362544f815eaf595004313e73192e15
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-07-04 18:09:03 -0500

    fixed scrolling, details coming together

--- script.js
+++ script.js
@@ -32,20 +32,35 @@ const colors = {
         const url = `https://pokeapi.co/api/v2/pokemon/${id}`
         const res = await fetch(url);
         const data = await res.json();
-        createPokemonCard(data);
+        createPokemonCard(data)
     }   
         createPokemonCard = (pokemon) => {
+            const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+
+            id = ""
+
+            if(pokemon.id < 10) {
+                id = "00" + pokemon.id
+            } else if(pokemon.id < 100) {
+                id = "0" + pokemon.id
+            } else {
+                id = pokemon.id
+            };
+
+            const poke_types = pokemon.types.map(type => type.type.name)
+            console.log(poke_types)
+
             const pokemonEl = document.createElement('div');
             pokemonEl.classList.add('pokemon');
 
             const pokemonInnerHTML = `    
             <div class="pokemon" style="background-color: rgb(222, 253, 224)">
             <div class="img-container">
-              <img src="https://archives.bulbagarden.net/media/upload/thumb/f/fb/0001Bulbasaur.png/600px-0001Bulbasaur.png" />
+              <img src="${pokemon.sprites.front_default}" />
             </div>
             <div class="info">
-              <span class="number">#001</span>
-              <h3 class="name">Bulbasaur</h3>
+              <span class="number">#${id}</span>
+              <h3 class="name">${name}</h3>
               <small class="type">Type: <span>grass</span></small>
             </div>
           </div>
